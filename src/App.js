790c6090aeb503baf320d89da177f0fa5d4e77c9
Johanna Palmkvist <johpa016@student.liu.se>
2024-01-13 19:08:53 +0100

    improvements + refactoring

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
+import Spiral from './Spiral';
 import './App.css';
 
 function App() {
 
-  const theText = 'Lorem ipsum dolor sit amet.';
-  const textArray = theText
-    .replaceAll(' ', '\xa0')
-    .split('');
+  const theText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+  const cycles = 3;
   const animationDuration = 10;
-  const cycles = 2;
 
   return (
-    <body>
-      <div className='spiralWrapper'>
-        {textArray.map((char, i) => 
-          <span className='char' key={i} style={{
-            animationDelay: `${-animationDuration/textArray.length * cycles * i}s`,
-            animationDuration: `${animationDuration}s`
-          }}>{char}</span>
-        )}
-      </div>
-      <div className='spiralWrapper'>
-        {textArray.map((char, i) => 
-          <span className='char' key={i} style={{
-            animationDelay: `${-animationDuration/textArray.length * cycles * i - animationDuration/2}s`,
-            animationDuration: `${animationDuration}s`
-          }}>{char}</span>
-        )}
-      </div>
-    </body>
+    <div className='App'>
+      <Spiral text={theText} cycles={cycles} duration={animationDuration} />
+      <Spiral text={theText} cycles={cycles} duration={animationDuration} delay={animationDuration / 2} />
+    </div>
   );
 }
 
---
+++ src/Spiral.js
@@ -0,0 +1,39 @@
+import { useRef, useEffect, useState } from 'react';
+import './Spiral.css';
+
+function Spiral({text = 'Insert your text in the text prop to see it spin', cycles = 2, duration = 5, delay = 0}) {
+  
+  const textArray = text
+    .replaceAll(' ', '\xa0')
+    .split('');
+  
+  const wrapperRef = useRef(null);
+  const [fontSize, setFontSize] = useState(`min(calc(100vw / ${textArray.length}), 50vh)`);
+  const [vpSize, setVpSize] = useState([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    const updateVpSize = () => {
+      console.log(window.innerHeight);
+      setVpSize([window.innerWidth, window.innerHeight])
+    };
+    window.addEventListener('resize', updateVpSize);
+    return () => window.removeEventListener('resize', updateVpSize);
+  }, [vpSize, textArray.length]);
+  useEffect(() => {
+    setFontSize(`min(${wrapperRef.current.getBoundingClientRect().width / textArray.length}px, 50vh)`);
+  }, [vpSize, textArray.length]);
+
+  return (
+    <div ref={wrapperRef} className='spiralWrapper'>
+      {textArray.map((char, i) => 
+        <span className='char' key={i} style={{
+          fontSize: fontSize,
+          animationDelay: `${-duration/textArray.length * cycles * i - delay}s`,
+          animationDuration: `${duration}s`
+        }}>{char}</span>
+      )}
+    </div>
+  );
+}
+
+export default Spiral;
